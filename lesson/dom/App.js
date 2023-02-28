680fd8781e957331c29c0ee6a1e0e5f55b18e688
duckiesakaminhduc <voducminh39@gmail.com>
2023-03-01 03:54:04 +0700

    commit

---
+++ lesson/dom/App.js
@@ -0,0 +1,6 @@
+let h1 = document.getElementsByTagName('h1');
+console.log(h1);
+document.querySelector('h1').style.color = 'red'
+let hightlight = document.querySelectorAll('ul .hightlight');
+console.log(hightlight)
+hightlight[0].style.color = 'yellow'
