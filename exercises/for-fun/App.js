3c028c60921566100ff2930fa1e1be955f70444b
duckiesakaminhduc <voducminh39@gmail.com>
2023-03-10 15:37:24 +0700

    commit cho vui

---
+++ exercises/for-fun/App.js
@@ -0,0 +1,9 @@
+const btnNode = document.getElementById('btn');
+const img1Node = document.getElementById('img1');
+
+btnNode.addEventListener('click', function () {
+    img1Node.setAttribute('src', "img/Lovepik_com-648397970-Vector light bulb glowing.png");
+});
+
+console.log(btnNode)
+console.log(img1Node)
